f4987272e27f6f8d71c5c11a0bc3de6dfdabd43b
pamodzoundiaye <mamodzou@hotmail.fr>
2017-02-09 09:30:38 +0100

    message

---
+++ Controllers/BotController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Syn.Bot.Siml;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace API.Controllers
+{
+    public class BotController : ApiController
+    {
+
+        [Route("api/bot/{name}")]
+        public IHttpActionResult Get(string name)
+        {
+            /*
+            var simlBot = new SimlBot();
+            var botUser = new BotUser(simlBot, "pamodzou");
+            var packageString = File.ReadAllText(@"C:\Users\Pamodzou Ndiaye\Documents\Visual Studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\anglais.simlpk");
+            simlBot.PackageManager.LoadFromString(packageString);
+             */
+            
+            var chatRequest = new ChatRequest(name, WebApiConfig.botUser);
+            var chatResult = WebApiConfig.simlBot.Chat(chatRequest);
+            var answer = chatResult.BotMessage;
+            return Ok(answer);
+        }
+    }
+}
---
+++ Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Syn.Bot.Siml;
+using System.IO;
+
+namespace API.Controllers
+{
+    public class MessageController : ApiController
+    {
+        Message[] messages = new Message[] {
+            new Message {Id="1",Content="Bonjour"},
+            new Message {Id="2",Content="Bien et toi"},
+            new Message {Id="3",Content="Au revoir"}
+        };
+        public IEnumerable<Message> GetAllMessages(){
+            return messages;
+        }
+        public IHttpActionResult GetMessage(string input){
+            var simlBot = new SimlBot();
+            var botUser = new BotUser(simlBot, "pamodzou");
+            var packageString = File.ReadAllText(@"C:\Users\Pamodzou Ndiaye\Documents\Visual Studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\anglais.simlpk");
+            simlBot.PackageManager.LoadFromString(packageString);
+            var chatRequest = new ChatRequest(input, botUser);
+            var chatResult = simlBot.Chat(chatRequest);
+            var answer = chatResult.BotMessage;
+            Console.WriteLine("reponse: "+answer);
+                //messages.FirstOrDefault((p) => p.Id == id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+            return Ok(answer);
+        }
+
+    }
+}
